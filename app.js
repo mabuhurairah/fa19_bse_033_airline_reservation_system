7a77a4d171c66502b48c50aa98a21ce968ce958c
mabuhurairah <abuhurairahishtiaq37@gmail.com>
2022-06-24 15:25:03 +0500

    React Install

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+});
+
 var indexRouter = require('./routes/index');
 
 var flightsRouter = require('./routes/api/flights');
